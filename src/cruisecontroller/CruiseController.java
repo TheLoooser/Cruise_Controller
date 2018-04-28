bb9ff7a99664ea1924d126afa9063097e12263cc
Dizzy <Dizzy@Super-Nova.home>
2018-04-28 15:28:16 +0200

    added line chart to the window

--- src/cruisecontroller/CruiseController.java
+++ src/cruisecontroller/CruiseController.java
@@ -21,6 +21,28 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import java.io.IOException;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -35,6 +57,19 @@ public class CruiseController extends Application implements Runnable {
     static double kp = 0.3;
     static double ki = 0.2;
     static double kd = 0.2;
+
+    /* line chart variables */
+    private static final int MAX_DATA_POINTS = 50;
+    private int xSeriesData = 0;
+    private XYChart.Series<Number, Number> series1 = new XYChart.Series<>();
+    private XYChart.Series<Number, Number> series2 = new XYChart.Series<>();
+    private XYChart.Series<Number, Number> series3 = new XYChart.Series<>();
+    private ExecutorService executor;
+    private ConcurrentLinkedQueue<Number> dataQ1 = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<Number> dataQ2 = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<Number> dataQ3 = new ConcurrentLinkedQueue<>();
+
+    private NumberAxis xAxis;
 
     HBox hbox = new HBox();
     BorderPane root = new BorderPane();
@@ -121,11 +156,11 @@ public class CruiseController extends Application implements Runnable {
         //---------END OF LEFT PART-------------
         //---------REMAINING PART--------------
         stage.setTitle("Animated Line Chart Sample");
+        init(stage);
         Scene scene = new Scene(root, 600, 300);
         stage.setScene(scene);
         stage.show();
         //--------END OF REMAINING-----------
-        
         CruiseController cc = new CruiseController();
         CarSimulator carSim = new CarSimulator();
         (new Thread(carSim)).start();
@@ -142,6 +177,22 @@ public class CruiseController extends Application implements Runnable {
 
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
+
+        //-------LINE CHART----------------
+        executor = Executors.newCachedThreadPool(new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                thread.setDaemon(true);
+                return thread;
+            }
+        });
+
+        AddToQueue addToQueue = new AddToQueue();
+        executor.execute(addToQueue);
+        //-- Prepare Timeline
+        prepareTimeline();
+        //--------END OF LINE CHART----------
 
         //handles the window closing event, stops simulation
         scene.getWindow().setOnCloseRequest(new EventHandler<javafx.stage.WindowEvent>() {
@@ -187,12 +238,101 @@ public class CruiseController extends Application implements Runnable {
         lastErr = error;
         lastTime = now;
     }
-    
+
     //this function shouldn't even be necessary
     public static void SetTunings(double Kp, double Ki, double Kd) {
         kp = Kp;
         ki = Ki;
         kd = Kd;
     }
+
+    //----------------------LINE CHART---------------------
+    private void init(Stage primaryStage) {
+
+        xAxis = new NumberAxis(0, MAX_DATA_POINTS, MAX_DATA_POINTS / 10);
+        xAxis.setForceZeroInRange(false);
+        xAxis.setAutoRanging(false);
+        xAxis.setTickLabelsVisible(false);
+        xAxis.setTickMarkVisible(false);
+        xAxis.setMinorTickVisible(false);
+
+        NumberAxis yAxis = new NumberAxis();
+
+        // Create a LineChart
+        final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis) {
+            // Override to remove symbols on each data point
+            @Override
+            protected void dataItemAdded(XYChart.Series<Number, Number> series, int itemIndex, XYChart.Data<Number, Number> item) {
+            }
+        };
+
+        lineChart.setAnimated(false);
+        lineChart.setTitle("Animated Line Chart");
+        lineChart.setHorizontalGridLinesVisible(true);
+
+        // Set Name for Series
+        series1.setName("Series 1");
+        series2.setName("Series 2");
+        series3.setName("Series 3");
+
+        // Add Chart Series
+        lineChart.getData().addAll(series1, series2, series3);
+
+//        primaryStage.setScene(new Scene(lineChart));
+        root.setRight(lineChart);
+    }
+
+    private class AddToQueue implements Runnable {
+
+        public void run() {
+            try {
+                // add a item of random data to queue
+                dataQ1.add(Math.random());
+                dataQ2.add(Math.random());
+                dataQ3.add(Math.random());
+
+                Thread.sleep(500);
+                executor.execute(this);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    //-- Timeline gets called in the JavaFX Main thread
+    private void prepareTimeline() {
+        // Every frame to take any data from queue and add to chart
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                addDataToSeries();
+            }
+        }.start();
+    }
+
+    private void addDataToSeries() {
+        for (int i = 0; i < 20; i++) { //-- add 20 numbers to the plot+
+            if (dataQ1.isEmpty()) {
+                break;
+            }
+            series1.getData().add(new XYChart.Data<>(xSeriesData++, dataQ1.remove()));
+            series2.getData().add(new XYChart.Data<>(xSeriesData++, dataQ2.remove()));
+            series3.getData().add(new XYChart.Data<>(xSeriesData++, dataQ3.remove()));
+        }
+        // remove points to keep us at no more than MAX_DATA_POINTS
+        if (series1.getData().size() > MAX_DATA_POINTS) {
+            series1.getData().remove(0, series1.getData().size() - MAX_DATA_POINTS);
+        }
+        if (series2.getData().size() > MAX_DATA_POINTS) {
+            series2.getData().remove(0, series2.getData().size() - MAX_DATA_POINTS);
+        }
+        if (series3.getData().size() > MAX_DATA_POINTS) {
+            series3.getData().remove(0, series3.getData().size() - MAX_DATA_POINTS);
+        }
+        // update
+        xAxis.setLowerBound(xSeriesData - MAX_DATA_POINTS);
+        xAxis.setUpperBound(xSeriesData - 1);
+    }
+    //----------------------END OF LINE CHART--------------
 
 }
